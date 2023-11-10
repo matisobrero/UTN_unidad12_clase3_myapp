934d4456908bc0d0da76799a5e126e2ca645b9a4
matisobrero <matias_sobrero@hotmail.com>
2023-11-10 16:25:22 -0300

    segundo commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,10 +3,10 @@ import './Main.css'
 
 function Main({producto}) {
   return (
-    <main className='principal'>  
-            <h1>PRODUCTO DESTACADO</h1>
-        <div className='container'>
-            <div className="row">
+<main className='principal'>  
+    <h1>PRODUCTO DESTACADO</h1>
+    <div className='container'>
+        <div className="row">
             <div className="card mb-3 ">
                 <img src={producto.imagen} className="card-img-top img-fluid imagen" alt="Imagen de producto"/>
                 <div className="card-body">
@@ -15,17 +15,16 @@ function Main({producto}) {
                     <p className="card-text"><small className="text-body-secondary">SKU: {producto.sku}</small></p>
                 </div>
                 <div>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item">{producto.disponible? 'Disponible' : 'Sin Stock'}</li>
-                    <li className="list-group-item">Precio: $ {producto.precio}</li>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">{producto.disponible? 'Disponible' : 'Sin Stock'}</li>
+                        <li className="list-group-item">Precio: $ {producto.precio}</li>
+                    </ul>
                     <button type="button" className="btn btn-warning">Comprar</button>
-                </ul>
-                </div>
-                
                 </div>
             </div>
         </div>
-    </main>
+    </div>
+</main>
   )
 }
 
